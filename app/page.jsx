ef8e027dedf44fc827ec8f73b7890dbe6acbeb31
danielarackal7 <daniel.arackal.3@gmail.com>
2025-06-18 01:32:03 -0500

    added statbox for org statistics, organized page to scale more fluidly.

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,28 @@
-import Image from 'next/image';
+'use client';
+
 import './globals.css';
 import RotatingWords from './components/RotatingWords';
 import PictureSlider from './components/PictureSlider';
 import Navbar from './components/Navbar';
-
+import StatBoxSection from './components/StatBox';
 
 export default function HomePage() {
   return (
     <div className="container">
       <Navbar />
-      <main className="main-content pt-28">
+
+      <main className="main-content pt-32 md:pt-36">
         <div className="text-section">
-          <h1 className="text-5xl font-bold leading-[4rem]">
-            Shaping <RotatingWords/> <br/>
-            in student leaders <br/>
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight md:leading-[4rem]">
+            Shaping <RotatingWords /> <br />
+            in student leaders <br />
             across Texas A&M
           </h1>
         </div>
-          <PictureSlider/>
-        <div/>
+        <PictureSlider />
       </main>
+
+      <StatBoxSection />
     </div>
   );
 }
